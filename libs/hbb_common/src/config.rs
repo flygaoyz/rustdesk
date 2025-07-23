51574fd190e999487eb7ee74fb23551d7ea56261
flygaoyz <44923170+flygaoyz@users.noreply.github.com>
2025-07-24 01:58:41 +0800

    Update config.rs
    修改设置固定密码错误

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -73,7 +73,7 @@ lazy_static::lazy_static! {
     pub static ref HARD_SETTINGS: RwLock<HashMap<String, String>> = {
         let mut map = HashMap::new();
         map.insert("password".to_string(),"qweQWE123!@#".to_string());
-        RwLock::new(map);
+        RwLock::new(map)
     };
     pub static ref BUILTIN_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
 }
